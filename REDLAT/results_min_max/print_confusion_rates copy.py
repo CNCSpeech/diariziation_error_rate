c7edf87072cc7db8efbb3a8153dcfc8a32d8e059
asosawelford <alejandrososawelford@gmail.com>
2024-07-26 14:46:08 -0300

    REDLAT PM and task DER breakdown

---
+++ REDLAT/results_min_max/print_confusion_rates copy.py
@@ -0,0 +1,23 @@
+import os
+
+RESULTS_FOLDER = '/home/aleph/diariziation_error_rate/REDLAT/results_min_max/'
+
+total_path = '/home/aleph/diariziation_error_rate/REDLAT/results_min_max/DER_min_max_n_speakers.csv'
+
+# print the total DER
+with open(total_path, 'r', encoding="utf-8") as f:
+    lines = f.readlines()
+    print("\nTotal Confusion rate is: ", round(float(lines[-1].split(',')[-1].strip()), 2), "%")
+
+for folder in os.listdir(RESULTS_FOLDER):
+    if os.path.isdir(os.path.join(RESULTS_FOLDER, folder)):
+        print("\n")
+        print(folder)
+        for filename in os.listdir(os.path.join(RESULTS_FOLDER, folder)):
+            if filename.endswith('.csv'):
+                #print the last line of the last column
+                with open(os.path.join(RESULTS_FOLDER, folder, filename), 'r', encoding="utf-8") as f:
+                    lines = f.readlines()
+                    print(filename.replace("DER_report_", "").replace(".csv", ""),"confusion rate: " ,
+                           round(float(lines[-1].split(',')[-1].strip()), 2), "%")
+        
